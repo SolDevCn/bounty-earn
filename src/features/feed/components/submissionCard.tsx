267fb44e4533610e9b263c77fcaba57b907b8546
0xDragon888 <lvxuan147@gmail.com>
2024-11-24 15:51:36 +0800

    fix listing

--- src/features/feed/components/submissionCard.tsx
+++ src/features/feed/components/submissionCard.tsx
@@ -96,7 +96,7 @@ export function SubmissionCard({ sub, type, commentCount }: SubCardProps) {
             pointerEvents: sub?.id || isProject ? 'all' : 'none',
           }}
         >
-          {isProject ? 'View Listing' : '浏览提交'}
+          {isProject ? '浏览提交' : '浏览提交'}
         </FeedCardLink>
       </Tooltip>
     </>
